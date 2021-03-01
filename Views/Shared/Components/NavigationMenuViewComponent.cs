7008b72ca7c429652e08679f81fb9d9903aa75cb
sowmyatellapragada <63146223+sowmyatellapragada@users.noreply.github.com>
2021-03-01 21:51:11 +0530

    Add files via upload

---
+++ Views/Shared/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SportsStore.Models;
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        //public string Invoke()
+        //{
+        //    return "Hello from the Nav View Component";
+        //}
+        private IStoreRepository repository;
+        public NavigationMenuViewComponent(IStoreRepository
+        repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x));
+        }
+    }
+}
